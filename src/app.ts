d40c1b666aad570f5c7a61b4af8688f52abba3b0
Huzaifa Ahmed <huzaifa.ahmed@bts.com>
2025-09-02 20:34:34 +0530

    add global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
-import express from 'express';
+import express, { type NextFunction, type Request, type Response } from 'express';
+import type { HttpError } from 'http-errors';
+import { config } from './config/config.js';
+import createHttpError from 'http-errors';
+import globalErrorHandler from './middlewares/globalErrorHandler.js';
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/', (req, res, next) => {
     res.send({ message: 'The api is working' });
 });
+
+// Global error handler
+app.use(globalErrorHandler);
 
 export default app;
