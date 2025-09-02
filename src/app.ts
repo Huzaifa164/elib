c5a4bf1c8a52843d4b42da28e1431b64457f28af
Huzaifa Ahmed <huzaifa.ahmed@bts.com>
2025-09-02 20:50:12 +0530

    create user router

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
-import express, { type NextFunction, type Request, type Response } from 'express';
-import type { HttpError } from 'http-errors';
-import { config } from './config/config.js';
-import createHttpError from 'http-errors';
+import express from 'express';
 import globalErrorHandler from './middlewares/globalErrorHandler.js';
+import userRouter from './user/userRouter.js';
 
 const app = express();
 
 app.get('/', (req, res, next) => {
     res.send({ message: 'The api is working' });
 });
+
+app.use('/api/users', userRouter);
 
 // Global error handler
 app.use(globalErrorHandler);
