c40017e171c022c9741f3341acba5ee84655c9e5
Huzaifa Ahmed <huzaifa.ahmed@bts.com>
2025-09-03 20:10:00 +0530

    add user model

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,5 +1,6 @@
 import type { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
+import userModel from "./userModel.js";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
     const { name, email, password } = req.body;
@@ -8,6 +9,15 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
         const error = createHttpError(400, "All fields are required");
         return next(error);
     }
+
+    // Database call
+    const user = await userModel.findOne({ email });
+
+    if (user) {
+        const error = createHttpError(400, "User already exist with this email.");
+        return next(error);
+    }
+
     res.json({ message: 'User created' });
 }
 
